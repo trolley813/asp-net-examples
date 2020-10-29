ffe0b7b244f5f758606830f8503655c3144107a6
trolley813 <trolleybus.1329@gmail.com>
2020-10-29 17:58:35 +0300

    added API controller

---
+++ 20200921/Controllers/ItemsAPIController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using _20200921.Data;
+using _20200921.Models;
+
+namespace _20200921.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsAPIController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ItemsAPIController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ItemsAPI
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return _context.Items;
+        }
+
+        // GET: api/ItemsAPI/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItem([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var item = await _context.Items.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // PUT: api/ItemsAPI/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItem([FromRoute] Guid id, [FromBody] Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ItemsAPI
+        [HttpPost]
+        public async Task<IActionResult> PostItem([FromBody] Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetItem", new { id = item.Id }, item);
+        }
+
+        // DELETE: api/ItemsAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return Ok(item);
+        }
+
+        private bool ItemExists(Guid id)
+        {
+            return _context.Items.Any(e => e.Id == id);
+        }
+    }
+}
